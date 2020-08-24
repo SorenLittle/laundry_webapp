b872906bda647361e8076ebd3a4a3b59080f2147
sorenlittle <sorenlittle.pdx@gmail.com>
2020-08-24 19:22:23 +0200

    successfully adding users and appointments into database

--- laundry_webapp/views/home.py
+++ laundry_webapp/views/home.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 from pyramid.view import view_config
 
 from laundry_webapp import models
@@ -7,14 +7,42 @@ from laundry_webapp import models
 @view_config(route_name='home',
              renderer='laundry_webapp:templates/home.pt')
 def home(request):
-    # if hasattr(request.POST.get('name'), 'text'):
-    #     name = request.POST.get('name')
-    #     appt = models.LeftAppt(date=date.today(), seven=name)
-    #     request.dbsession.add(appt)
+    # TODO: distinguish between left and right machines -> change "appointment = models..." line
 
-    name = request.POST.getall('name')
-    appt = models.LeftAppt(date=date.today(), seven=name)
-    request.dbsession.add(appt)
+    # execute when there is a POST request
+    if request.POST.get('name'):
+
+        # receive a "given_name" from the users POST into the website
+        given_name = request.POST.get('name')
+        # TODO: add other kinds of string cleaning
+        given_name = given_name.lower()
+        given_id = ''
+
+        # get the appropriate id from the database
+        first_time = True
+        for user_id, name in request.dbsession.query(models.User.id, models.User.name):
+            if given_name == name:
+                first_time = False
+                given_id = user_id
+                break
+
+        # if the user is not in the database, add them
+        if first_time:
+            request.dbsession.add(models.User(name=given_name))
+
+            # get that new users id from the database
+            for user_id, name in request.dbsession.query(models.User.id, models.User.name):
+                if given_name == name:
+                    given_id = user_id
+                    break
+
+        # build appointment for that user
+        appointment = models.Appointment(datetime=datetime.today().replace(hour=7),
+                                         user_id=given_id,
+                                         machine_id="Left",
+                                         )
+
+        request.dbsession.add(appointment)
 
     return {
         'url': request.route_url('home')
